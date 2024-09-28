ff1989c1072c9e4b92e04ac509067578a6e6ffd5
vishali <pavishali21@gamil.com>
2024-09-28 22:03:23 +0530

    initial commit

---
+++ src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.tinder_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+}
